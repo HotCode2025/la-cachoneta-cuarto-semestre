933a5bbca38c0a43c48a73118afd07c5f6b468c0
Emmanuel Yapura <eyapura96@gmail.com>
2026-08-11 00:14:16 -0300

    crea e-commerce clase 1 javascript

---
+++ programacion IV/javascript/e-commerce2026/client/js/products.js
@@ -0,0 +1,45 @@
+const products = [
+    {
+        id: 1,
+        productName: "Banana",
+        price: 480,
+        quanty: 1,
+        img: "/client/media/banana.jpg"
+    },
+    {
+        id: 2,
+        productName: "Leche",
+        price: 950,
+        quanty: 6,
+        img: "/client/media/leche.jpg"
+    },
+    {
+        id: 3,
+        productName: "Pollo",
+        price: 750,
+        quanty: 7,
+        img: "/client/media/pollo.jpg"
+    },
+    {
+        id: 4,
+        productName: "Mayonesa",
+        price: 520,
+        quanty: 4,
+        img: "/client/media/mayonesa.jpg"
+    },
+    {
+        id: 5,
+        productName: "enlatados",
+        price: 160,
+        quanty: 12,
+        img: "/client/media/enlatados.jpg"
+    },
+    {
+        id: 6,
+        productName: "varios",
+        price: 200,
+        quanty: 1,
+        img: "/client/media/varios.jpg"
+    }
+
+]
