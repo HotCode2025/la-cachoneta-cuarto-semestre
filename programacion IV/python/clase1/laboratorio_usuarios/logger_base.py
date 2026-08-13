4dbccc391987ffed87146b66ee33854ba424ebf9
Emmanuel Yapura <eyapura96@gmail.com>
2026-08-12 21:15:08 -0300

    modifica nivel del log a INFO

--- programacion IV/python/clase1/laboratorio_usuarios/logger_base.py
+++ programacion IV/python/clase1/laboratorio_usuarios/logger_base.py
@@ -6,9 +6,10 @@ carpeta_actual = os.path.dirname(os.path.abspath(__file__))
 # Ruta completa del archivo de log
 ruta_log = os.path.join(carpeta_actual, "laboratorio_usuarios.log")
 
-log.basicConfig(level=log.DEBUG,
+log.basicConfig(level=log.INFO,
                 format='%(asctime)s:%(levelname)s [%(filename)s:%(lineno)s] %(message)s',
                 datefmt='%I:%M:%S %p',
                 handlers=[
-                  log.FileHandler(ruta_log)
+                  log.FileHandler(ruta_log),
+                  log.StreamHandler()
                 ])
