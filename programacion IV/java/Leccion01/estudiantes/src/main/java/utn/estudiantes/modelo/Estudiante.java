6a6f94a8ad0bf53153f6653901ac842f437519a4
Emmanuel Yapura <eyapura96@gmail.com>
2026-09-03 01:33:30 -0300

    agrega clase 3 java

---
+++ programacion IV/java/Leccion01/estudiantes/src/main/java/utn/estudiantes/modelo/Estudiante.java
@@ -0,0 +1,31 @@
+package utn.estudiantes.modelo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+// Representa una tabla de datos, es una clase de entidad
+@Entity
+// boilerplate - Repetitivo
+//Genera los get y set
+@Data
+// Constructor vacio
+@NoArgsConstructor
+// Constructor con todos los argumentos
+@AllArgsConstructor
+@ToString
+public class Estudiante  {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idEstudiante;
+    private String nombre;
+    private String apellido;
+    private String telefono;
+    private String email;
+}
