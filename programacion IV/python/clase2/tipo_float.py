d8c94b7aa6f2d8a046c7aed551bc08f2fbbb6890
Enzo Ulloa <enzo.ulloa.i@gmail.com>
2026-09-17 01:06:52 -0300

    agrego clase 2 de python

---
+++ programacion IV/python/clase2/tipo_float.py
@@ -0,0 +1,20 @@
+# Profundizando en el tipo float
+a = 3.0
+print(f'a: {a:.2f}')
+
+# Constructor de tipo float - puede recibir int y str
+a = float(10)  # Le pasamos un tipo entero (int)
+a = float('10')  # Le pasamos un tipo cadena (str)
+print(f'a: {a:.2f}')
+
+# Notacion exponencial (valores positivos y negativos)
+a = 3e5
+print(f'a: {a:.2f}')
+
+a = 3e-5
+print(f'a: {a:.5f}')
+
+# Cualquier calculo que incluye un float, todo cambia a float
+a = 4.0 + 5
+print(a)
+print(type)
